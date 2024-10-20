23e81d1338192b56d98f1ea353d318dccc1dec1e
William <victorwccv@gmail.com>
2024-10-20 18:05:22 -0500

    fix: Se agregaron estilos para Full responsive

--- src/pages/orderSummary/components/CartSummary.tsx
+++ src/pages/orderSummary/components/CartSummary.tsx
@@ -1,12 +1,11 @@
 import styles from "../css/CartSummary.module.css";
-import {useAppState} from "../../../hooks/useAppState";
+import { useAppState } from "../../../hooks/useAppState";
 import { type CartItem } from "../../../types/interfaces/product.interface";
 import { Link } from "react-router-dom";
 
 function CartSummary() {
   const { state, dispatch } = useAppState();
   const { cartItems } = state;
-
 
   const handleRemoveFromCart = (item: CartItem) => {
     dispatch({ type: "REMOVE_ITEM_FROM_CART", payload: item });
@@ -20,7 +19,6 @@ function CartSummary() {
     dispatch({ type: "INCREASE_QUANTITY", payload: item });
   };
 
-  
   return (
     <div className={styles.cartSummaryContainer}>
       <table className={styles.cartSummary}>
@@ -28,7 +26,7 @@ function CartSummary() {
           <tr>
             <td>Producto</td>
             <td>Nombre</td>
-            <td>Precio Un</td>
+            <td>Precio Und.</td>
             <td>Cantidad</td>
             <td>Total</td>
             <td>Eliminar</td>
@@ -39,16 +37,18 @@ function CartSummary() {
             <>
               {cartItems.map((item) => (
                 <tr key={item.id}>
-                  <td>
+                  <td data-label="Producto">
                     <img
                       src={item.image}
                       alt={item.title}
                       className={styles.product__image}
                     />
                   </td>
-                  <td title={item.title}>{item.title}</td>
-                  <td>$ {item.price}</td>
-                  <td>
+                  <td title={item.title} data-label="Nombre">
+                    {item.title}
+                  </td >
+                  <td data-label="Precio und.">$ {item.price}</td>
+                  <td data-label="Cantidad">
                     <span className={styles.cartSummary__quantity}>
                       <button
                         type="button"
@@ -65,8 +65,8 @@ function CartSummary() {
                       </button>
                     </span>
                   </td>
-                  <td>$ {item.total.toFixed(2)}</td>
-                  <td>
+                  <td data-label="Total">$ {item.total.toFixed(2)}</td>
+                  <td >
                     <button
                       type="button"
                       className={styles.cartSummary__deleteButton}
@@ -78,8 +78,8 @@ function CartSummary() {
                 </tr>
               ))}
               <tr className={styles.cartSummary__grandTotal}>
-                <td colSpan={4}>Total a pagar:</td>
-                <td colSpan={2}>
+                <td colSpan={3}>Total a pagar:</td>
+                <td colSpan={3}>
                   ${" "}
                   {cartItems
                     .reduce((acc, item) => acc + item.total, 0)
